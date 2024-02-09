a18065f80222b808237bd21a610a3174e084f7f8
Siddhant Raj <siddhantraj@Siddhants-MacBook-Air.local>
2024-02-09 19:42:13 +0530

    Add Content to  SignUp and Login Component

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,11 +1,47 @@
-import React from 'react'
-
+import React from "react";
+import NavBar from "../components/NavBar";
+import { Link } from "react-router-dom";
+import "./../styles/Home.css";
 const Login = () => {
   return (
     <div>
-      login
-    </div>
-  )
-}
+      <NavBar />
+      <div className="mainContainer">
+        <div className="secureHeading">
+          <div className="name">
+            <span className="firstLetter">L</span>ogin
+          </div>
+          
 
-export default Login
+        </div>
+        <div className="form">
+            <label for="name">Name : </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Siddhant"
+              autocomplete="on"
+              required
+            />
+            <label for="email">Email ID : </label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="xyz@gmail.com"
+              autocomplete="on"
+              required
+            />
+            <label for="password">Password : </label>
+            <input type="password" name="password" id="password" required />
+            <button class="signupBtn1">Login</button>
+          </div>
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,11 +1,54 @@
-import React from 'react'
-
+import React from "react";
+import NavBar from "../components/NavBar";
+import { Link } from "react-router-dom";
+import "./../styles/Home.css";
 const SignUp = () => {
   return (
     <div>
-      signup
-    </div>
-  )
-}
+      <NavBar />
+      <div className="mainContainer">
+        <div className="secureHeading">
+          <div className="name">
+            <span className="firstLetter">S</span>ign Up
+          </div>
+          
 
-export default SignUp
+        </div>
+        <div className="form">
+            <label for="name">Name : </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Siddhant"
+              autocomplete="on"
+              required
+            />
+            <label for="email">Email ID : </label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="xyz@gmail.com"
+              autocomplete="on"
+              required
+            />
+            <label for="password">Password : </label>
+            <input type="password" name="password" id="password" required />
+            <label for="passwordConfirm">Confirm Password : </label>
+            <input
+              type="password"
+              name="passwordConfirm"
+              id="passwordConfirm"
+              required
+            />
+            <button class="signupBtn1">Sign Up</button>
+          </div>
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
