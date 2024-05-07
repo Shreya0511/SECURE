5cc70d0cca438262e0e37edee238039837570c80
Shreya Singh <singh.shreya0511@gmail.com>
2024-05-07 09:41:25 +0530

    edit sensor

--- frontend/src/components/EditForm.js
+++ frontend/src/components/EditForm.js
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from "react";
 import { AuthData } from "../services/AuthService";
 import Form from "react-bootstrap/Form";
 import { useNavigate } from "react-router-dom";
+import moment from "moment";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
 
 const EditForm = () => {
   const navigate = useNavigate();
@@ -12,6 +17,11 @@ const EditForm = () => {
   const [model, setModel] = useState("");
   const [threshold, setThreshold] = useState("");
   const [status, setStatus] = useState(false);
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Please enter the aimed montly cost of the sensor in rupees.
+    </Tooltip>
+  );
 
   useEffect(() => {
     const senor = JSON.parse(user.user).sensors.find(
@@ -26,7 +36,7 @@ const EditForm = () => {
   }, [selectedSensor, user.user]);
 
   const handleSaveChanges = async (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
     const sensorData = {
       sensorId,
       manufacturer,
@@ -52,7 +62,7 @@ const EditForm = () => {
       const data = await response.json();
       if (data.status === "success") {
         alert("Sensor data updated successfully");
-        navigate('/dashboard', { replace: true });
+        navigate("/dashboard", { replace: true });
         window.location.reload();
       } else {
         alert("Failed to update sensor data");
@@ -67,10 +77,17 @@ const EditForm = () => {
     navigate("/dashboard");
   };
 
+  const handleThresCalc = (event) => {
+    const thresh =
+      (event.target.value * 1000) / (moment().daysInMonth() * 24 * 8);
+    setThreshold(thresh.toFixed(2));
+  };
+
   return (
     <div style={{ color: "white", marginTop: "3rem" }}>
       <div style={{ color: "white", fontSize: "1.7rem", fontWeight: "bold" }}>
-        Edit Sensor: <span style={{ color: "rgb(222, 143, 83)" }}>{sensorId}</span>
+        Edit Sensor:{" "}
+        <span style={{ color: "rgb(222, 143, 83)" }}>{sensorId}</span>
       </div>
       <form
         style={{
@@ -82,37 +99,115 @@ const EditForm = () => {
       >
         {/* Form inputs */}
         {/* Sensor Id */}
-        <div style={{ display: "flex", flexDirection: "row", marginBottom: "1rem" }}>
-          <label style={{ width: "12rem", fontWeight: "bold" }}>Sensor Id:</label>
-          <Form.Control type="text" value={sensorId} onChange={(e) => setSensorId(e.target.value)} />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginBottom: "1rem",
+          }}
+        >
+          <label style={{ width: "12rem", fontWeight: "bold" }}>
+            Sensor Id:
+          </label>
+          <Form.Control
+            type="text"
+            value={sensorId}
+            onChange={(e) => setSensorId(e.target.value)}
+          />
         </div>
 
         {/* Manufacturer */}
-        <div style={{ display: "flex", flexDirection: "row", marginBottom: "1rem" }}>
-          <label style={{ width: "12rem", fontWeight: "bold" }}>Manufacturer:</label>
-          <Form.Control type="text" value={manufacturer} onChange={(e) => setManufacturer(e.target.value)} />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginBottom: "1rem",
+          }}
+        >
+          <label style={{ width: "12rem", fontWeight: "bold" }}>
+            Manufacturer:
+          </label>
+          <Form.Control
+            type="text"
+            value={manufacturer}
+            onChange={(e) => setManufacturer(e.target.value)}
+          />
         </div>
 
         {/* Model */}
-        <div style={{ display: "flex", flexDirection: "row", marginBottom: "1rem" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginBottom: "1rem",
+          }}
+        >
           <label style={{ width: "12rem", fontWeight: "bold" }}>Model:</label>
-          <Form.Control type="text" value={model} onChange={(e) => setModel(e.target.value)} />
+          <Form.Control
+            type="text"
+            value={model}
+            onChange={(e) => setModel(e.target.value)}
+          />
         </div>
 
         {/* Parameter */}
-        <div style={{ display: "flex", flexDirection: "row", marginBottom: "1rem" }}>
-          <label style={{ width: "12rem", fontWeight: "bold" }}>Parameter:</label>
-          <Form.Control type="text" value={parameter} onChange={(e) => setParameter(e.target.value)} />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginBottom: "1rem",
+          }}
+        >
+          <label style={{ width: "12rem", fontWeight: "bold" }}>
+            Parameter:
+          </label>
+          <Form.Control
+            type="text"
+            value={parameter}
+            onChange={(e) => setParameter(e.target.value)}
+          />
         </div>
 
         {/* Threshold */}
-        <div style={{ display: "flex", flexDirection: "row", marginBottom: "1rem" }}>
-          <label style={{ width: "12rem", fontWeight: "bold" }}>Threshold:</label>
-          <Form.Control type="text" value={threshold} onChange={(e) => setThreshold(e.target.value)} />
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginBottom: "1rem",
+          }}
+        >
+          <label style={{ width: "13.5rem", fontWeight: "bold", flex: "2" }}>
+            Monthly Cost:
+          </label>
+          <Form.Control
+            type="text"
+            value={threshold}
+            onChange={handleThresCalc}
+            style={{ flex: "4", marginRight: "1rem" }}
+          />
+          <OverlayTrigger
+            placement="right"
+            delay={{ show: 250, hide: 400 }}
+            overlay={renderTooltip}
+          >
+            <div>
+              <FontAwesomeIcon
+                style={{ fontSize: "1.3rem", color: "rgb(222, 143, 83)" }}
+                icon={faCircleExclamation}
+              />
+            </div>
+          </OverlayTrigger>
         </div>
 
         {/* Status */}
-        <div style={{ display: "flex", flexDirection: "row", marginBottom: "1rem" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            marginBottom: "1rem",
+          }}
+        >
           <label style={{ width: "8rem", fontWeight: "bold" }}>Status:</label>
           <div style={{ marginRight: "1rem" }}>
             <input
@@ -133,7 +228,14 @@ const EditForm = () => {
         </div>
 
         {/* Buttons */}
-        <div style={{ display: "flex", flexDirection: "row", alignItems: "center", marginTop: "1rem" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            marginTop: "1rem",
+          }}
+        >
           <button
             type="submit" // Add type submit to trigger form submission
             style={{
